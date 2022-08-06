21fa74bcfc175d397c01c49ca38da414794db9a0
Ekaterina Korotkova <e.korotkova.2013@yandex.ru>
2022-08-06 18:16:51 +0300

    initial commit

---
+++ Otus.SpanningTree.Kruskal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Otus.SpanningTree.Kruskal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
